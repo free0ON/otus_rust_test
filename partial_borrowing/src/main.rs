1dc687fc5283d305d103727a4319a1d198dad0b0
free0ON <8333184@gmail.com>
2025-12-18 04:48:48 +0300

    play aroun partial boggoring

--- partial_borrowing/src/main.rs
+++ partial_borrowing/src/main.rs
@@ -1,15 +1,29 @@
-struct Point {
-    x: f64,
-    y: f64,
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
-impl Point {
-    pub fn x_mut(&mut self) -> &mut f64 {
+impl<'a, T> Point<T>
+//where T: Copy,
+{
+    pub fn x_mut(&mut self) -> &'a mut T {
         &mut self.x
     }
 
-    pub fn y_mut(&mut self) -> &mut f64 {
+    pub fn y_mut(&mut self) -> &'a mut T {
         &mut self.y
+    }
+}
+
+struct PointTuple<T>(T, T); // x, y
+
+impl<T> PointTuple<T> {
+    pub fn x_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+
+    pub fn y_mut(&mut self) -> &mut T {
+        &mut self.1
     }
 }
 
@@ -17,24 +31,36 @@ impl Point {
 //use crate::Point;
 #[test]
 fn borrow_test() {
-    let mut point: Point = Point { x: 1.0, y: 1.0 };
+    let mut point: Point<i32> = Point::<i32> { x: 1, y: 1 };
     let ref mut x_ref = point.x;
     let ref mut y_ref = point.y;
-    *x_ref *= 2.0;
-    *y_ref *= 2.0;
-    assert_eq!(*x_ref, 2.0);
-    assert_eq!(*y_ref, 2.0);
+    *x_ref *= 2;
+    *y_ref *= 2;
+    assert_eq!(*x_ref, 2);
+    assert_eq!(*y_ref, 2);
 }
 
 #[cfg(feature = "partial_borrowing")]
 #[test]
-#[ignore = "cannot partial borrow"]
+//#[ignore = "cannot partial borrow"]
 fn partial_borrow_test() {
-    let mut point: Point = Point { x: 1.0, y: 1.0 };
-    let x_mut = point.x_mut();
-    let y_mut = point.y_mut();
+    let mut point: Point<i32> = Point::<i32> { x: 1, y: 1 };
+    let mut x_mut = point.x_mut();
+    let mut y_mut = point.y_mut();
+
+    let mut point_tuple = PointTuple::<i32>(1, 1);
+    let &mut mut x_tuple_mut = point_tuple.x_mut();
+    let &mut mut y_tuple_mut = point_tuple.y_mut();
+
     //          ^~~~~
     // error: cannot borrow `point` as mutable more than once at a time
-    *x_mut *= 2.0;
-    *y_mut *= 2.0;
+    *x_mut = 2;
+    *y_mut = 2;
+    assert_eq!(point.x, 2);
+    assert_eq!(point.y, 2);
+
+    x_tuple_mut = 2;
+    y_tuple_mut = 2;
+    assert_eq!(point_tuple.0, 1);
+    assert_eq!(point_tuple.1, 1);
 }
